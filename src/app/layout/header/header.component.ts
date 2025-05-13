18966db8579f7abd9c4b7eaf2eb86ba57a0a54e3
KerlosGirgis <kerlos0@protonmail.com>
2025-05-13 09:04:56 +0300

    some fixes for the header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -4,68 +4,103 @@ import {
   EventEmitter,
   OnChanges,
   SimpleChanges,
-} from '@angular/core';
+  OnInit,
+} from "@angular/core";
 import {
   Router,
   RouterEvent,
   RouterLink,
   RouterLinkActive,
-} from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ApiService } from '../../core/services/api.service';
-import { AuthService } from '../../core/services/auth.service';
-import { SearchService } from '../../core/services/search.service';
-import { SidebarService } from '../../core/services/sidebar.service';
+} from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { ApiService } from "../../core/services/api.service";
+import { AuthService } from "../../core/services/auth.service";
+import { SearchService } from "../../core/services/search.service";
+import { SidebarService } from "../../core/services/sidebar.service";
+import { Auth, authState, user, User } from "@angular/fire/auth";
+import { firstValueFrom } from "rxjs";
+import { doc, Firestore, getDoc } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
 })
-export class HeaderComponent {
-  currenturl: string = '';
-  searchquery: string = '';
+export class HeaderComponent implements OnInit, OnChanges {
+  currenturl: string = "";
+  searchquery: string = "";
   isSearch: boolean = false;
-  isloged: boolean = false;
+  isloged: number = 0;
+  initial: String = "";
+  wishlistLength: number = 0;
   constructor(
     public sidebarService: SidebarService,
     private router: Router,
     private movieService: ApiService,
     private searchService: SearchService,
-    private authService: AuthService
-  ) {
-    this.isloged = this.authService.isLogged();
+    private authService: AuthService,
+    private auth: Auth,
+    private firestore: Firestore
+  ) {}
+  async ngOnChanges(changes: SimpleChanges): Promise<void> {
+    const user = await this.getCurrentUser();
+    if (user != null) {
+      const userRef = doc(this.firestore, `users/${user.uid}`);
+      const userSnap = await getDoc(userRef);
+      this.initial = userSnap.data()?.["name"].charAt(0);
+      this.wishlistLength = userSnap.data()?.["wishlist"].length;
+      this.isloged = 1;
+    } else {
+      this.isloged = 2;
+    }
+  }
+  async ngOnInit(): Promise<void> {
+    const user = await this.getCurrentUser();
+    if (user != null) {
+      const userRef = doc(this.firestore, `users/${user.uid}`);
+      const userSnap = await getDoc(userRef);
+      this.initial = userSnap.data()?.["name"].charAt(0);
+      this.wishlistLength = userSnap.data()?.["wishlist"].length;
+      this.isloged = 1;
+    } else {
+      this.isloged = 2;
+    }
   }
 
+  private async getCurrentUser(): Promise<User | null> {
+    return await firstValueFrom(authState(this.auth));
+  }
   getCurrentUrl() {
-    if (this.router.url !== '/movies/search') {
+    if (this.router.url !== "/movies/search") {
       this.currenturl = this.router.url;
       console.log(this.currenturl);
     }
   }
- search(): void {
-  const query = this.searchquery.trim();
+  search(): void {
+    const query = this.searchquery.trim();
 
-  if (query) {
-    this.isSearch = true;
-    this.searchService.updateValue(query);
-    console.log('Search:', query);
-    this.router.navigate(['/movies/search']);
-  } else if (this.isSearch) {
+    if (query) {
+      this.isSearch = true;
+      this.searchService.updateValue(query);
+      console.log("Search:", query);
+      this.router.navigate(["/movies/search"]);
+    } else if (this.isSearch) {
+      this.clearSearch();
+    }
+  }
+
+  reset(): void {
     this.clearSearch();
   }
-}
 
-reset(): void {
-  this.clearSearch();
+  private clearSearch(): void {
+    this.isSearch = false;
+    this.searchquery = "";
+    this.searchService.updateValue("");
+    if (this.router.url == "/movies/search") {
+      this.router.navigate([this.currenturl]);
+    }
+  }
 }
-
-private clearSearch(): void {
-  this.isSearch = false;
-  this.searchquery = '';
-    this.searchService.updateValue('');
-    if (this.router.url=='/movies/search') {
-  this.router.navigate([this.currenturl]);}
-}}
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,46 +1,91 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { AuthService } from '../../core/services/auth.service';
-import { SidebarService } from '../../core/services/sidebar.service';
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from "../../core/services/auth.service";
+import { SidebarService } from "../../core/services/sidebar.service";
+import { Auth, authState, User } from "@angular/fire/auth";
+import { firstValueFrom } from "rxjs";
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [CommonModule,RouterLink,RouterLinkActive],
+  selector: "app-sidebar",
+  imports: [CommonModule, RouterLink, RouterLinkActive],
 
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"],
 })
-export class SidebarComponent {
-isloged :boolean = false;
+export class SidebarComponent implements OnInit,OnChanges {
+  isloged: boolean = false;
 
-  constructor(public sidebarService: SidebarService , private authService:AuthService) {
-    this.isloged = this.authService.isLogged();
+  constructor(
+    public sidebarService: SidebarService,
+    private authService: AuthService,
+    private auth:Auth
+  ) {}
+  async ngOnChanges(): Promise<void> {
+    const user = await this.getCurrentUser();
+    if(user!=null){
+      this.isloged=true;
+    }
+    else{
+      this.isloged=false;
+    }
   }
-
-selectedSection: number = 1;
+  async ngOnInit(): Promise<void> {
+        const user = await this.getCurrentUser();
+    if(user!=null){
+      this.isloged=true;
+    }
+    else{
+      this.isloged=false;
+    }
+  }
+    private async getCurrentUser(): Promise<User | null> {
+      return await firstValueFrom(authState(this.auth));
+    }
+  selectedSection: number = 1;
   section: ISection[] = [
-    {icon: '🏠 ', name: 'Home', id: 1 ,href:"/movies"},
-    { icon: '🎥 ', name: 'Now Playing', id: 2, href: '/movies/movies-categories', param: 'now_playing' },
-    { icon: '🔥 ', name: 'Popular', id: 3, href: '/movies/movies-categories',param: 'popular' },
-    { icon: '⭐ ', name: 'Top Rated', id: 4, href: '/movies/movies-categories',param: 'top_rated' },
-    { icon: '📡 ', name: 'Upcoming', id: 5, href: '/movies/movies-categories',param: 'upcoming' },
+    { icon: "🏠 ", name: "Home", id: 1, href: "/movies" },
+    {
+      icon: "🎥 ",
+      name: "Now Playing",
+      id: 2,
+      href: "/movies/movies-categories",
+      param: "now_playing",
+    },
+    {
+      icon: "🔥 ",
+      name: "Popular",
+      id: 3,
+      href: "/movies/movies-categories",
+      param: "popular",
+    },
+    {
+      icon: "⭐ ",
+      name: "Top Rated",
+      id: 4,
+      href: "/movies/movies-categories",
+      param: "top_rated",
+    },
+    {
+      icon: "📡 ",
+      name: "Upcoming",
+      id: 5,
+      href: "/movies/movies-categories",
+      param: "upcoming",
+    },
   ];
   navigate(id: number) {
-this.selectedSection= id
+    this.selectedSection = id;
   }
-  logout(){
+  logout() {
     this.authService.logout();
     location.reload();
-
-  };
-
+  }
 }
 interface ISection {
-  icon:string
+  icon: string;
   name: string;
   id: number;
   href: string;
-  param?: string
+  param?: string;
 }
-
